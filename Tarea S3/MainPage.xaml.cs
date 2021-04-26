7270c27a279ebef821f72ca8600c0c3716c8068c
luism <luismigez@hotmail.com>
2021-04-26 15:33:22 -0500

    Agregar archivos de proyecto.

---
+++ Tarea S3/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea_S3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private  async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUser.Text;
+            string clave = txtPass.Text;
+            if (usuario == "estudiante2021" && clave == "uisrael2021")
+            {
+                await Navigation.PushAsync(new Page2());
+            }
+            else
+            {
+                DisplayAlert("ERROR", "Usuario o Contrasenia Incorrecta", "Gracias");
+            }
+            
+                
+
+            
+        }
+    }
+}
---
+++ Tarea S3/Page2.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tarea_S3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPromedio_Clicked(object sender, EventArgs e)
+        {
+            string esta = Convert.ToString(txtEstado);
+            double Nota1 = Convert.ToDouble(txtNota1.Text);
+            double Exa1 = Convert.ToDouble(txtExamen1.Text);
+            double N1 = Nota1 * 0.3;
+            double E1 = Exa1 * 0.2;
+            double P1 = N1 + E1;
+            txtNotaP1.Text = P1.ToString();
+
+            double Nota2 = Convert.ToDouble(txtNota2.Text);
+            double Exa2 = Convert.ToDouble(txtExamen2.Text);
+            double N2 = Nota2 * 0.3;
+            double E2 = Exa2 * 0.2;
+            double P2 = N2 + E2;
+            txtNotaP2.Text = P2.ToString();
+
+            double NF = P1 + P2;
+            txtNotaFinal.Text = NF.ToString();
+
+            if (NF >= 7)
+            {
+                esta = "Aprobado".ToString();
+            }
+            if (NF >= 5 && NF <= 6.9)
+            {
+                esta = "Complementario".ToString();
+            }
+            if (NF < 4.9)
+            {
+                esta = "Reprobado".ToString();
+            }
+
+            txtEstado.Text = esta;
+
+        }
+    }
+}
